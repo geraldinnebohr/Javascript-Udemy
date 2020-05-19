64b48e29d9219005be61f67340439651b065b5fc
geraldinnebohr <geraldinnebohr@gmail.com>
2020-05-18 21:29:13 -0500

    [FEATURE] 43 classes

---
+++ fundamentals/app.js
@@ -0,0 +1,15 @@
+document.getElementById('app').innerHTML = `Bienvenido años`;
+
+// concat strings
+let string = 'Java' + 'Script';
+console.log(string);
+
+let object = [1,2,3];
+console.log(object);
+
+let other = new Array();
+console.log(other);
+
+let date = new Date();
+console.log(date);
+console.log(date.getTime());
